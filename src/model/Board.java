1f27ee821785c17457f9d7be3d2fde7c5e4388e9
Daniel <Daniel@Prtatil>
2014-11-13 17:50:12 +0000

    method GetSquare STILL returns null. STILL no idea why. STILL Must fix.

--- src/model/Board.java
+++ src/model/Board.java
@@ -2,17 +2,17 @@ package model;
 
 public class Board {
 
-    private Square[][] board;
+    public final Square[][] board;
 
     public Board(int size) {
-        board = new Square[size][size];
+       board = new Square[size][size];
     }
 
     public int getSize() {
         return board.length;
     }
 
-    public Square getSquare(int pos1, int pos2) {
-        return board[pos1][pos2];
+    public Square getSquare(int x, int y) {
+        return board[x][y];
     }
 }
